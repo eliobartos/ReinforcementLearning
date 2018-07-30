bf5d9122cc358f699c6dd11e400ec3283436207b
Elio Bartoš <eliobartos@yahoo.com>
2018-07-30 23:45:21 +0200

    Added results of 1M simulations, improved visualization

--- Learning_SimpleBlackjack.R
+++ Learning_SimpleBlackjack.R
@@ -1,7 +1,7 @@
-library()
+#setwd("C:/Users/Elio/Documents/R/Reinforcement Learning - David Silver/ReinforcementLearning")
 source("Simulator.R")
 source("ControlFunctions.R")
-
+library(microbenchmark)
 ## Creating epsilon decay function
 length(Simple_simulator$states)*length(Simple_simulator$actions)
 
@@ -13,15 +13,32 @@ epsilon_fun = function(k){
   }
 }
 
+epsilon_fun2 = function(k){
+  if(k <= 20000) {
+    return(1)
+  } else if (k <= 800000) {
+    return(1/sqrt(sqrt(sqrt(k-20000))))
+  } else {
+    return(1/k)
+  }
+}
+
 vec_epsilon_fun = Vectorize(epsilon_fun)
+vec_epsilon_fun2 = Vectorize(epsilon_fun2)
 
 
-x = 1:100000
-y = vec_epsilon_fun(1:100000)
+x = 1:1000000
+y = vec_epsilon_fun(1:1000000)
+y2 = vec_epsilon_fun2(1:1000000)
 
-plot(x,y)
+quantile(y2)
+
+plot(x,y, type = "l")
+lines(x, y2, col = "blue")
 
 ### Learning
+load("SimpleBlackjackResults_100k.RData")
+
 microbenchmark(
   res <- GLIE_MC_control(states = Simple_simulator$states,
                         actions = Simple_simulator$actions,
@@ -32,7 +49,18 @@ microbenchmark(
 )
 
 res_100k = res
-save(res_100k, file = "SimpleBlackjackResults_100k.RData")
+
+microbenchmark(
+  res <- GLIE_MC_control(states = Simple_simulator$states,
+                         actions = Simple_simulator$actions,
+                         simulator = Simple_simulator$simulator,
+                         n_sim = 1000000,
+                         epsilon_fun = epsilon_fun2),
+  times = 1L
+)
+
+res_1m = res
+save(res_100k, res_1m, file = "SimpleBlackjackResults.RData")
 
 
 
--- VisualizeResults.R
+++ VisualizeResults.R
@@ -1,51 +1,63 @@
+#setwd("C:/Users/Elio/Documents/R/Reinforcement Learning - David Silver/ReinforcementLearning")
 library(ggplot2)
 library(tidyverse)
-load("SimpleBlackjackResults_100k.RData")
+library(gridExtra)
+library(extrafont)
+
+
+loadfonts(device = "win")
+
+load("SimpleBlackjackResults.RData")
 
 print(res_100k)
 
 
+res = res_1m
+
 ## Process Policy
-policy = res_100k$policy
+policy = res$policy
 policy = data.frame(policy)
 policy$state = rownames(policy)
 policy = gather(policy, action, take_action, hit, stand)
 
-Q = res_100k$Q
+Q = res$Q
 Q = data.frame(Q)
 Q$state = rownames(Q)
 Q = Q %>% gather(action, pct_win, hit, stand)
 
-N = res_100k$N
+N = res$N
 N = data.frame(N)
 N$state = rownames(N)
 N = N %>% gather(action, times_visited, hit, stand)
 
 results = policy %>% merge(Q) %>% merge(N)
 results = results %>% 
-  filter(times_visited > 0,
-         !grepl("21", state)) %>% 
+  filter(times_visited > 0) %>% 
+         #filter(!grepl("21", state)) %>% 
   mutate(take_action = as.factor(take_action),
          is_soft = grepl("soft", state),
          order = as.numeric(gsub("([0-9]+).*", "\\1", c(state))))
 
-
+#### Two plot solution
 hard = results %>% 
   filter(!is_soft)
 
 p1 = ggplot(hard, aes(x = action, 
                  y = reorder(state, order, mean))) + 
   geom_tile(aes(color = take_action, fill = take_action)) +
-  geom_text(aes(label = paste0(round(pct_win*100,1), "%"))) +
+  geom_text(aes(label = paste0(round(pct_win*100,1), "%")),
+            color = "gray10", fontface = "bold") +
   ylab("") +
   xlab("") +
   ggtitle("Hard") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(labels = c("Don't", "Take"), values = c("red", "skyblue")) +
-  scale_fill_manual(labels = c("Don't", "Take"), values = c("red", "skyblue")) +
-  guides(fill=guide_legend(title="Take action?"),
-         color = guide_legend(title="Take action?"))
-
+  scale_color_manual(values = c("grey35", "grey35")) +
+  scale_fill_manual(values = c("hotpink1", "skyblue1")) +
+  guides(fill= FALSE,
+         color = FALSE) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 20),
+        text = element_text(face = "bold", family = "Courier New"))
+p1
 
 soft = results %>% 
   filter(is_soft)
@@ -53,15 +65,18 @@ soft = results %>%
 p2 = ggplot(soft, aes(x = action, 
                  y = reorder(state, order, mean))) + 
   geom_tile(aes(color = take_action, fill = take_action)) +
-  geom_text(aes(label = paste0(round(pct_win*100,1), "%"))) +
+  geom_text(aes(label = paste0(round(pct_win*100,1), "%")),
+            color = "gray10", fontface = "bold") +
   ylab("") +
   xlab("") +
   ggtitle("Soft") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(labels = c("Don't", "Take"), values = c("red", "skyblue")) +
-  scale_fill_manual(labels = c("Don't", "Take"), values = c("red", "skyblue")) +
+  scale_color_manual(labels = c("Don't", "Take"), values = c("grey35", "grey35")) +
+  scale_fill_manual(labels = c("Don't", "Take"), values = c("hotpink1", "skyblue1")) +
   guides(fill=guide_legend(title="Take action?"),
-         color = guide_legend(title="Take action?"))
+         color = guide_legend(title="Take action?")) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 20),
+        text = element_text(face = "bold", family = "Courier New"))
+p2
+grid.arrange(p1, p2, nrow = 1, widths = c(0.80, 1))
 
-library(gridExtra)
-grid.arrange(p1, p2, nrow = 1)
